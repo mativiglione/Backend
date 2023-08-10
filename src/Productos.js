6c89c21b6b2006c6d36abbd039d104e9e03a86b2
Matias Viglione <mativiglione22@gmail.com>
2023-08-10 16:51:42 -0300

    cambios server con express

--- src/Productos.js
+++ src/Productos.js
@@ -13,9 +13,7 @@ export class ProductManager {
       const data = await fs.readFile(this.filePath, "utf8");
       this.products = JSON.parse(data);
       this.idCounter = this.calculateIdCounter();
-    } catch (error) {
-      console.error("Error al inicializar", error);
-    }
+    } catch (error) {}
   }
 
   calculateIdCounter() {
@@ -37,4 +35,95 @@ export class ProductManager {
       console.error("Error al escribir en el archivo:", error);
     }
   }
+
+  addProduct(title, description, price, thumbnail, code, stock) {
+    const product = {
+      id: this.idCounter,
+      title: title,
+      description: description,
+      price: price,
+      thumbnail: thumbnail,
+      code: code,
+      stock: stock,
+    };
+
+    this.idCounter++;
+    this.products.push(product);
+    this.writeDataToFile();
+  }
+
+  updateProduct(id, field, value) {
+    const product = this.products.find((product) => product.id === id);
+
+    if (product) {
+      product[field] = value;
+      this.writeDataToFile();
+    } else {
+      console.log("Producto no encontrado");
+    }
+  }
+
+  deleteProduct(id) {
+    const index = this.products.findIndex((product) => product.id === id);
+
+    if (index !== -1) {
+      this.products.splice(index, 1);
+      this.writeDataToFile();
+      console.log("Producto borrado");
+    } else {
+      console.log("Producto no encontrado");
+    }
+  }
+
+  getProductById(id) {
+    const product = this.products.find((product) => product.id === id);
+
+    if (product) {
+      return product;
+    } else {
+      console.log("Producto no encontrado");
+    }
+  }
 }
+
+const filePath = "./src/products.json";
+
+(async () => {
+  const getProducts = new ProductManager(filePath);
+
+  await getProducts.addProduct(
+    "EA FC 24",
+    "La experiencia futbolística más fiel hasta la fecha.",
+    20000,
+    "Sin imagen",
+    "abc123",
+    50
+  );
+
+  await getProducts.addProduct(
+    "Counter-Strike: Global Offensive",
+    "CS:GO amplía el juego de acción por equipos del que fue pionero cuando salió hace más de 20 años.",
+    5000,
+    "Sin imagen",
+    "fgh456",
+    0
+  );
+
+  await getProducts.addProduct(
+    "Street Fighter 6",
+    "Street Fighter 6 hace gala de la potencia del RE ENGINE de Capcom, e incluye tres modos de juego: World Tour, Fighting Ground y Battle Hub.",
+    17000,
+    "Sin imagen",
+    "asd656",
+    15
+  );
+
+  await getProducts.addProduct(
+    "NBA 2k24",
+    "Experimenta la cultura del baloncesto en NBA 2K24.",
+    15000,
+    "Sin imagen",
+    "jkg656",
+    50
+  );
+})();
--- src/app.js
+++ src/app.js
@@ -1,79 +1,39 @@
-import express from "express";
-import fs from "fs/promises";
+import express from 'express';
+import fs from 'fs/promises';
 
-import { ProductManager } from "./Productos.js";
+import { ProductManager } from './Productos.js';
 
 const app = express();
 const port = 4000;
 
-const filePath = "./src/products.json";
+const filePath = './products.json';
 const ProductManagerInstance = new ProductManager(filePath);
 
 app.use(express.json());
 
-ProductManagerInstance.addProduct(
-  "EA FC 24",
-  "La experiencia futbolística más fiel hasta la fecha.",
-  20000,
-  "Sin imagen",
-  "abc123",
-  50
-);
+app.get('/products', async (req, res) => {
+    const limit = req.query.limit;
+    let products = ProductManagerInstance.getProducts();
 
-ProductManagerInstance.addProduct(
-  "Counter-Strike: Global Offensive",
-  "CS:GO amplía el juego de acción por equipos del que fue pionero cuando salió hace más de 20 años.",
-  5000,
-  "Sin imagen",
-  "fgh456",
-  0
-);
+    if (limit) {
+      const parsedLimit = parseInt(limit, 0);
+      products = products.slice(0, parsedLimit);
+    }
 
-ProductManagerInstance.addProduct(
-  "Street Fighter 6",
-  "Street Fighter 6 hace gala de la potencia del RE ENGINE de Capcom, e incluye tres modos de juego: World Tour, Fighting Ground y Battle Hub.",
-  17000,
-  "Sin imagen",
-  "asd656",
-  15
-);
+    res.json(products);
+  });
 
-ProductManagerInstance.addProduct(
-  "NBA 2k24",
-  "Experimenta la cultura del baloncesto en NBA 2K24.",
-  15000,
-  "Sin imagen",
-  "jkg656",
-  50
-);
+  app.get('/products/:pid', async (req, res) => {
+    const productId = parseInt(req.params.pid);
+    const product = ProductManagerInstance.getProductById(productId);
 
-ProductManagerInstance.updateProduct(1, "price", 25000);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ error: 'Producto no encontrado' });
+    }
+  });
 
-ProductManagerInstance.deleteProduct(2);
-
-app.get("/products", async (req, res) => {
-  const limit = req.query.limit;
-  let products = ProductManagerInstance.getProducts();
-
-  if (limit) {
-    const parsedLimit = parseInt(limit, 3);
-    products = products.slice(0, parsedLimit);
-  }
-
-  res.json(products);
-});
-
-app.get("/products/:pid", async (req, res) => {
-  const productId = parseInt(req.params.pid);
-  const product = ProductManagerInstance.getProductById(productId);
-
-  if (product) {
-    res.json(product);
-  } else {
-    res.status(404).json({ error: "Producto no encontrado" });
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Servidor abierto en http://localhost:${port}`);
-});
+  app.listen(port, () => {
+    console.log(`Servidor abierto en http://localhost:${port}`);
+  });
