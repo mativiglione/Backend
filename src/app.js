f8ada29e3de7021cf76449955ad8e8c01a8a263f
Matias Viglione <mativiglione22@gmail.com>
2023-08-11 14:55:32 -0300

    correcion tercer entraga

--- src/app.js
+++ src/app.js
@@ -1,39 +1,40 @@
-import express from 'express';
-import fs from 'fs/promises';
+import express from "express";
+import fs from "fs/promises";
 
-import { ProductManager } from './Productos.js';
+import { ProductManager } from "./Productos.js";
 
 const app = express();
 const port = 4000;
 
-const filePath = './src/products.json';
+const filePath = "./src/products.json";
 const ProductManagerInstance = new ProductManager(filePath);
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/products', async (req, res) => {
-    const limit = req.query.limit;
-    let products = await ProductManagerInstance.getProducts();
+app.get("/products", async (req, res) => {
+  const limit = req.query.limit;
+  let products = await ProductManagerInstance.getProducts();
 
-    if (limit) {
-      const parsedLimit = parseInt(limit, 5);
-      products = products.slice(0, parsedLimit);
-    }
+  if (limit) {
+    const parsedLimit = parseInt(limit, 5);
+    products = products.slice(0, parsedLimit);
+  }
 
-    res.json(products);
-  });
+  res.json(products);
+});
 
-  app.get('/products/:pid', async (req, res) => {
-    const productId = parseInt(req.params.pid);
-    const product = ProductManagerInstance.getProductById(productId);
+app.get("/products/:pid", async (req, res) => {
+  const productId = parseInt(req.params.pid);
+  const product = await ProductManagerInstance.getProductById(productId);
 
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404).json({ error: 'Producto no encontrado' });
-    }
-  });
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ error: "Producto no encontrado" });
+  }
+});
 
-  app.listen(port, () => {
-    console.log(`Servidor abierto en http://localhost:${port}`);
-  });
+app.listen(port, () => {
+  console.log(`Servidor abierto en http://localhost:${port}`);
+});
